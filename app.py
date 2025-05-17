b619d932afec16362fc2561104cb3fd3f8eb6c01
laidleyt <tom.laidley@gmail.com>
2025-05-17 11:38:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -211,9 +211,10 @@ def update_graph_and_subhead(selected_data, selected_graph):
 
     return fig, subhead
 
+server = app
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, port=8050)
 
 
 
